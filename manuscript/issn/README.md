37a1ca2e3586c1b1f0613260d50fc19cfbba3cbe
Roderic Page <rdmpage@gmail.com>
2013-06-21 16:34:59 +0100

    Manuscript

---
+++ manuscript/issn/README.md
@@ -0,0 +1,14 @@
+ISSN
+====
+
+Distribution of articles over time, grouped by ISSN
+
+CouchDB query
+
+http://direct.bionames.org:5984/bionames/_design/issn/_view/year?group_level=2
+
+Saved as issn.json
+
+The run issn.php to generate tab-delimited dump
+
+Plot in Excel
---
+++ manuscript/issn/issn.php
@@ -0,0 +1,73 @@
+<?php
+
+$json = file_get_contents('issn.json');
+
+$obj = json_decode($json);
+
+// count
+
+$total = array();
+
+foreach ($obj->rows as $row)
+{
+	if (!isset($row->key[0]))
+	{
+		$total[$row->key[0]] = 0;
+	}
+	$total[$row->key[0]] += $row->value;
+}
+
+//rsort($total);
+//print_r($total);
+
+$limit = 1000;
+
+$table_rows = array();
+
+foreach ($total as $issn => $count)
+{
+	if ($count > $limit)
+	{
+		for ($decade = 1750; $decade < 2020; $decade += 10)
+		{
+			if (!isset($table_rows[$decade]))
+			{
+				$table_rows[$decade] = array();
+			}
+			$table_rows[$decade][$issn] = 0;
+		}
+	}
+}
+
+foreach ($obj->rows as $row)
+{
+	$issn = $row->key[0];
+	if ($total[$issn] > $limit)
+	{
+		$decade = $row->key[1];
+		$table_rows[$decade][$issn] = $row->value;
+	}
+}	
+
+//print_r($table_rows);
+
+// text dump
+
+// header
+$journals = array_keys($table_rows['1750']);
+
+echo "\t" . join("\t", $journals) . "\n";
+
+foreach ($table_rows as $decade => $row)
+{
+	echo $decade;
+	foreach ($row as $count)
+	{
+		echo "\t" . $count;
+	}
+	echo "\n";
+}
+
+
+?>
+
